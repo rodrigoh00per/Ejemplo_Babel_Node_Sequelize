c5e2bb6d300d19f5a8eca06777128f0bc324aa21
rodrigoh00per <rodrigoh00per1@gmail.com>
2019-07-26 00:09:31 -0500

    cambios en el puerto

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import app from "./app";
 import "@babel/polyfill";
 
 async function main(){
-    await app.listen();
+    await app.listen(process.env.PORT);
     console.log("servidor levantado en el puerto 3000");
 }
 
