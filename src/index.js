43c37e2aaa3c15f36ea242b74e24a0f7be651461
rodrigoh00per <rodrigoh00per1@gmail.com>
2019-07-26 00:10:07 -0500

    cambio

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import app from "./app";
 import "@babel/polyfill";
 
-async function main(){
-    await app.listen(process.env.PORT);
-    console.log("servidor levantado en el puerto 3000");
+async function main() {
+  await app.listen(process.env.PORT, () => {
+    console.log("servidor levantado");
+  });
 }
 
-
 main();
-
